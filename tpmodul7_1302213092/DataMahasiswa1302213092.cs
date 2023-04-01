3d80531a4ba4c3422d6e62cdccaa5a50bb50fb73
hifahir <putratertuahirzi@gmail.com>
2023-04-02 06:22:18 +0700

    menambahkan class <NAMA_CLASS>

---
+++ tpmodul7_1302213092/DataMahasiswa1302213092.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.Json;
+namespace tpmodul7_1302213092;
+
+public class DataMahasiswa1302213092
+{
+   Mahasiswa mahasiswa = new Mahasiswa();
+
+    public DataMahasiswa1302213092()
+    {
+
+    }
+
+    string text = File.ReadAllText("C:\\Kuliah\\Semester 4\\Konstruksi Perangkat Lunak\\GitHub\\tpmodul7_1302213092\\tpmodul7_1302213092\\tp7_1_13002213092.json");
+
+    public void readJson()
+    {
+        mahasiswa = JsonSerializer.Deserialize<Mahasiswa>(text);
+        Console.WriteLine($"Nama: {mahasiswa.nama.depan} {mahasiswa.nama.belakang}");
+        Console.WriteLine($"NIM: {mahasiswa.nim}");
+        Console.WriteLine($"Fakultas: {mahasiswa.fakultas}");
+    }
+}
---
+++ tpmodul7_1302213092/Mahasiswa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace tpmodul7_1302213092;
+
+public class Mahasiswa
+{
+	public Mahasiswa()
+	{
+
+	}
+
+	public Nama nama { get; set; }
+	public long nim { get; set; }
+	public string fakultas { get; set; }
+}
