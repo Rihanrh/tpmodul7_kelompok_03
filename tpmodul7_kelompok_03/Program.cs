ad9f334c67fd10aaed52ae240d0a7c8a7daf7657
MrLI0N7 <93116969+MrLI0N7@users.noreply.github.com>
2023-04-02 13:14:44 +0800

    Menambahkan class DataMahasiswa_1302213037
    berisi kelas DataMahasiswa_1302213037 yang memiliki procedure readJSON yang membaca file JSON, ada pun beberapa kelas tambahan pada kelas ini yaitu kelas Nama yang akan menampung nama depan, belakang dan Kelas Mahasiswa yang ada nama, nim dan fakultas dari suatu suatu objek Mahasiswa.

--- tpmodul7_kelompok_03/Program.cs
+++ tpmodul7_kelompok_03/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul7_kelompok_03;
+
+DataMahasiswa_1302213037 mhs = new DataMahasiswa_1302213037();
+mhs.ReadJSON();
+
