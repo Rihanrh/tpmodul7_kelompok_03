e92ff72f23051e8a4d4505bf75dccc5c48623809
hifahir <putratertuahirzi@gmail.com>
2023-04-02 07:20:40 +0700

    menambahkan class KuliahMahasiswa1302213092

---
+++ tpmodul7_1302213092/Course.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace tpmodul7_1302213092;
+
+public class Course
+{
+	public string code { get; set; }
+	public string name { get; set; }
+	public Course()
+	{
+	}
+}
---
+++ tpmodul7_1302213092/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace tpmodul7_1302213092;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        //DataMahasiswa1302213092 data = new DataMahasiswa1302213092();
+        //data.readJson();
+        KuliahMahasiswa1302213092 coolyeah = new KuliahMahasiswa1302213092();
+        coolyeah.readJSON();
+        
+    }
+}
