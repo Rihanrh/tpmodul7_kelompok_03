1aa2f3c8955b23a80e8d571e67c38fef84603e47
jaatsiyah <jaatsiyah@student.telkomuniversity.ac.id>
2023-04-02 13:15:28 +0700

    menambahkan class DataMahasiswa1302213043

--- tpmodul7_kelompok_03/Program.cs
+++ tpmodul7_kelompok_03/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        DataMahasiswa_1302213043 dataMahasiswa = new DataMahasiswa_1302213043();
+        dataMahasiswa.ReadJSON();
+    }
+}
