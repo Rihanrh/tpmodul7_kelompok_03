8e7139fa40c1e131eba73f38bcbb896a407e64f4
Rihanrh <rihanh@student.telkomuniversity.ac.id>
2023-04-02 12:39:00 +0700

    menambahkan class KuliahMahasiswa1302210028

---
+++ tpmodul7_kelompok_03/KuliahMahasiswa1302210028.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace tpmodul7_kelompok_03
+{
+    public class Kmhs
+    {
+        public List<Course> courses { get; set; }
+        public Kmhs() { }
+
+        public Kmhs(List<Course> courses)
+        {
+            this.courses = courses;
+        }
+    }
+
+    public class Course
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+
+        public Course() { }
+
+        public Course(string code, string name)
+        {
+            this.code = code;
+            this.name = name;
+        }
+    }
+
+    internal class KuliahMahasiswa1302210028
+    {
+        public void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("C:\\Users\\RH\\Documents\\GitHub\\tpmodul7_kelompok_03\\tpmodul7_kelompok_03\\tp7_2_1302210028.json");
+            Kmhs course = JsonSerializer.Deserialize<Kmhs>(jsonString);
+            int i = 1;
+            foreach (Course courses in course.courses)
+            {
+                Console.WriteLine("MK " + i + courses.code + " - " + courses.code);
+                i++;
+            }
+        }
+    }
+}
--- tpmodul7_kelompok_03/Program.cs
+++ tpmodul7_kelompok_03/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using tpmodul7_kelompok_03;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        KuliahMahasiswa1302210028 kmhs = new KuliahMahasiswa1302210028();
+        kmhs.ReadJSON();
+    }
+}
